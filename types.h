f7c73fea69f8fe88598c3bf1484803e67f3c309d
tobecomeawind <tobecomeawind@github.com>
2024-10-01 12:04:09 +0300

    adding types

---
+++ types.h
@@ -0,0 +1,18 @@
+
+typedef enum {
+	CHAR  = 0b00000001,  // idea for char type 
+						 // the last bit says the type == char
+						 // the another bits says about size, 
+						 // including the first bit
+						 // 
+						 // if bit another bytes are zero
+						 // 	size == 1 byte
+						 // else 
+						 // 	size == CHAR(for example 0b01110001)
+						 //                  char with size 2^4 + 2^5 + 2^6
+						 
+	INT   = 0b00000010,
+	FLOAT = 0b00000100	
+} vtypes; // variable types
+
+
