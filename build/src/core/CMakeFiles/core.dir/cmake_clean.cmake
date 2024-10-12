a350d41c9b58b6cbb4caccf108d30e25f446fdc5
tobecomeawind <tobecomeawind@github.com>
2024-10-12 12:00:54 +0300

    Delete build folder

--- build/src/core/CMakeFiles/core.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcore.a"
-  "libcore.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/core.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/src/core/relation/CMakeFiles/relation.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for relation.
-# This may be replaced when dependencies are built.
